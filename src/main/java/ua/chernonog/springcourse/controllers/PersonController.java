7d2fa25757f20ff347654fb3735380003c6a9c7b
skillfuldev <chernonog.evgeniy@gmail.com>
2023-04-17 14:44:40 +0300

    add_pageHome

--- src/main/java/ua/chernonog/springcourse/controllers/PersonController.java
+++ src/main/java/ua/chernonog/springcourse/controllers/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ua.chernonog.springcourse.dao.PersonDAO;
+import ua.chernonog.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -26,6 +27,11 @@ public class PersonController {
     public String getPerson(@PathVariable("id") int id, Model model){
         model.addAttribute("person",personDAO.getPerson(id));
         return "/people/person";
+    }
+    @GetMapping("/new")
+    public String newPersonPage(Model model){
+        model.addAttribute("person", new Person());
+        return "/people/new";
     }
 
 
